5c3451634a9d86b2e4e2293529c95e5ae5064479
jobaer-ahmed <ajobaer08@gmail.com>
2022-08-20 00:09:44 +0800

    tax added

--- js/utilities.js
+++ js/utilities.js
@@ -5,12 +5,24 @@ function getTextElementValueById(elementId) {
     return currentPhoneTotal;
 }
 
+function setTextElementValueById(elementId, value) {
+    const subTotalElement = document.getElementById(elementId);
+    subTotalElement.innerText = value;
+}
+
 function calclulateSubTotal() {
     //calculate total
     const currentPhoneTotal = getTextElementValueById('phone-total');
     const currentCaseTotal = getTextElementValueById('case-total');
 
     const currentSubTotal = currentPhoneTotal + currentCaseTotal;
-    const subTotalElement = document.getElementById('sub-total');
-    subTotalElement.innerText = currentSubTotal;
+    setTextElementValueById('sub-total', currentSubTotal);
+
+    // calcluate tax
+    const taxAmountString = (currentSubTotal * .01).toFixed(2);
+    const taxAmount = parseFloat(taxAmountString);
+    setTextElementValueById('tax-amount', taxAmount);
+
+    const finalSubTotal = currentSubTotal + taxAmount;
+    setTextElementValueById('final-total', finalSubTotal)
 }
